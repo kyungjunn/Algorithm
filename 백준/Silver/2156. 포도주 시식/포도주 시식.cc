0a15526f97d6031f9daac806e93add9892cac4c6
KimKyungJun <1124kyungjun@naver.com>
2026-01-26 16:13:21 +0900

    [Silver I] Title: 포도주 시식, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int wine[10001], dp[10001];
+
+void dynamic()
+{
+    dp[0] = 0;
+    dp[1] = wine[1];
+    dp[2] = wine[1] + wine[2];
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = max({dp[i-3] + wine[i-1] + wine[i], dp[i-2] + wine[i], dp[i-1]}); 
+    }
+    
+}
+int main()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> wine[i];    
+    }
+    dynamic();
+    cout << dp[n];
+    return 0;
+}
